9954b1c9b3824a2dca025af4b8c5bf91bf68176b
Ali Hasan <ali.hasan@northsouth.edu>
2021-10-17 00:11:58 +0600

    added global states

--- budgetApp/src/Context/GlobalState.js
+++ budgetApp/src/Context/GlobalState.js
@@ -1,5 +1,16 @@
  import React, {createContext, useReducer} from 'react'
 
  const initialState ={
-     
- }
+     incomeTransaction:[
+         {id:1, incomeText: "Salary", incomeAmount: "500"},
+         {id:2, incomeText: "Savings", incomeAmount: "5000"},
+         {id:3, incomeText: "Profit", incomeAmount: "50000"},
+     ],
+     expenseTransaction:[
+         {id:1, expenseText: "Bazar", expenseAmount: "500"},
+         {id:2, expenseText: "rent", expenseAmount: "5000"},
+         {id:3, expenseText: "Daily Cost", expenseAmount: "50000"},
+     ]
+
+ };
+ export const GlobaContext = createContext(initialState);
